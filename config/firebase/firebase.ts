06866c7ec29c725c7573769eba685be606aa98ee
John Okely <jlokely@gmail.com>
2021-02-18 08:11:04 +0800

    Move to my new one

--- config/firebase/firebase.ts
+++ config/firebase/firebase.ts
@@ -1,21 +1,21 @@
-import firebase from 'firebase/app';
-import 'firebase/auth';
-import 'firebase/firestore';
+import firebase from "firebase/app";
+import "firebase/auth";
+import "firebase/firestore";
 
 const config = {
-    apiKey: process.env.FIREBASE_API_KEY,
-    authDomain: 'peakseekers-b6502.firebaseapp.com',
-    databaseURL: 'https://peakseekers-b6502.firebaseio.com',
-    projectId: 'peakseekers-b6502',
-    storageBucket: 'peakseekers-b6502.appspot.com',
-    messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
-    appId: process.env.FIREBASE_APP_ID,
-    measurementId: process.env.FIREBASE_MEASUREMENT_ID,
+  apiKey: process.env.FIREBASE_API_KEY,
+  authDomain: "sonnet-4df2e.firebaseapp.com",
+  databaseURL: "https://sonnet-4df2e.firebaseio.com",
+  projectId: "sonnet-4df2e",
+  storageBucket: "sonnet-4df2e.appspot.com",
+  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.FIREBASE_APP_ID,
+  measurementId: process.env.FIREBASE_MEASUREMENT_ID,
 };
 
 if (!firebase.apps.length) {
-    //initializing with the config object
-    firebase.initializeApp(config);
+  //initializing with the config object
+  firebase.initializeApp(config);
 }
 
 //separting database API and authentication
